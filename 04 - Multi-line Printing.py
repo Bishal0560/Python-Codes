e19a178b7f044ee0fd290271ba8e0c2daa74e894
ShreyasKashyap357 <118651908+ShreyasKashyap357@users.noreply.github.com>
2022-11-24 10:27:31 +0530

    Create 04 - Multi-line Printing.py

---
+++ 04 - Multi-line Printing.py
@@ -0,0 +1,3 @@
+print("Name - Shreyas Kashyap \n"+"Roll Number - 31 \n"+"City - Mumbai \n")
+# \n is an "escape sequence" to move the "cursor" (during execution) to the next line
+# "+" is used for concatenation, and joins 2 strings together, without any spaces
