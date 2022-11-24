1f9c9b84abcba3d5dc1a0ccf80a1a6a6cb5abcb3
ShreyasKashyap357 <118651908+ShreyasKashyap357@users.noreply.github.com>
2022-11-24 10:23:39 +0530

    Create 01 - Printing (Single & Multiple).py

---
+++ 01 - Printing (Single & Multiple).py
@@ -0,0 +1,12 @@
+print("hello")  # printing function
+print("ello "*3)  # printing same string 3 times - for printing a string n times back-to-back without spaces, use "(string characters)"*n
+print("take care")
+print("bye")
+# my name is...
+# this is a single line comment
+"""this is one example
+of a multiple line comment"""
+'''this is another example
+of a multiple line comment.
+Note that multiple line comments in between the code is not a good practice as it is a docstring and can be executable
+Use multiple single line comments instead.'''
