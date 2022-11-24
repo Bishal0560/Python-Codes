8f04bd801115e2fe5835172c5684f674e9038667
ShreyasKashyap357 <118651908+ShreyasKashyap357@users.noreply.github.com>
2022-11-24 11:03:34 +0530

    Add files via upload

---
+++ 06 - Output Question for Variable Assignment.py
@@ -0,0 +1,10 @@
+# question on variable declaration and overwriting value for requested output
+# Store "Hello" in 3 variables. Print the values of the 3 variables in three lines using only a single print statement.
+# Then print 99, Hello Python and Hello World in three lines using a single print statement, without putting the strings
+# in the print statement.
+val1 = val2 = val3 = "Hello"
+print(val1, "\n", val2, "\n", val3)
+val1 = 99
+val2 = "Hello Python"
+val3 = "Hello World"
+print(val1, "\n", val2, "\n", val3)
