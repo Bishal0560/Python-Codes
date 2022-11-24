ad7dceb7128bd6a509dd7464f2d660f615ddb076
ShreyasKashyap357 <118651908+ShreyasKashyap357@users.noreply.github.com>
2022-11-24 10:25:04 +0530

    Create 02 - Single Line Comments.py

---
+++ 02 - Single Line Comments.py
@@ -0,0 +1,12 @@
+print("Name - Shreyas Kashyap")  # This is my name
+print("10th Marks - 96.2%")  # This is my 10th marks
+print("12th Marks - 70.67%")  # This is my 12th Marks
+print("Roll Number - 31")  # This is my roll number
+print()
+print("Alternate Commentary")
+print()
+# I am printing my name, 10th marks, 12th marks, and roll number
+print("Name - Shreyas Kashyap")
+print("10th Marks - 96.2%")
+print("12th Marks - 70.67%")
+print("Roll Number - 31")
