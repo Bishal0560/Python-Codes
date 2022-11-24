e6e9bc619628bea11e7323cde055e77a1214fd31
ShreyasKashyap357 <118651908+ShreyasKashyap357@users.noreply.github.com>
2022-11-24 10:28:37 +0530

    Create 05 - Variable Declaration & Assignment.py

---
+++ 05 - Variable Declaration & Assignment.py
@@ -0,0 +1,12 @@
+# variable declaration and value assignment
+name = "Shreyas Kashyap"  # declaration of a variable with variable name as "name" and value as "Shreyas Kashyap".
+# Data type is assigned automatically, no need to explicitly assign data type unless required.
+section = "ABC"
+rollno = 31
+percentage = 99.999
+print(name, "\n", section, "\n", rollno, "\n", percentage)
+name = 678  # variable value has been updated. Note that the data type has changed. Variables are dynamic,
+# their data types as well as values can be changed any time.
+section = 22
+print()
+print(name, "\t", section)
